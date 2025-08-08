97b51db3c30bdc7f6d85ac2e92973fe7f02a5b88
souradeep1122 <147180736+Saha-family@users.noreply.github.com>
2025-08-09 00:52:33 +0530

    adding files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const { url } = require('inspector');
 const app = express();
+const fs = require('fs');
 
 const multer = require('multer');
 
@@ -32,8 +33,23 @@ app.post('/upload', upload.single("profile_img"),(req,res) =>{
     console.log(req.body)
     console.log(req.file)
 
-    return res.redirect("/");
+    return res.redirect("/files")
 
 })
+
+
+// Show uploaded files
+app.get('/files', (req, res) => {
+  const uploadsDir = path.join(__dirname, 'uploads');
+  fs.readdir(uploadsDir, (err, files) => {
+    if (err) {
+      console.error('Error reading uploads directory:', err);
+      return res.status(500).send('Unable to read uploads directory');
+    }
+    const visibleFiles = (files || []).filter(f => !f.startsWith('.'));
+    res.render('files', { files: visibleFiles });
+  });
+});
+
 
 app.listen(3000, () => console.log('Server running on port 3000'));
